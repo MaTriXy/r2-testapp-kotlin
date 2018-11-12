fba3904d1acb8f579ee6a3ab1bfabd0883ef6ef7
Paul Stoica <stoica.paul92@gmail.com>
2018-11-12 13:11:04 +0100

    added pause speech button

--- r2-testapp/src/main/java/org/readium/r2/testapp/R2ScreenReader.kt
+++ r2-testapp/src/main/java/org/readium/r2/testapp/R2ScreenReader.kt
@@ -12,6 +12,7 @@ package org.readium.r2.testapp
 
 import android.content.Context
 import android.speech.tts.TextToSpeech
+import android.speech.tts.UtteranceProgressListener
 import android.text.Html
 import android.widget.Toast
 import org.readium.r2.shared.Publication
@@ -20,16 +21,29 @@ import java.net.URL
 import java.util.*
 
 
+/**
+ * R2ScreenReader
+ *
+ * A basic screen reader based on Android's TextToSpeech
+ *
+ *
+ */
+
+
 class R2ScreenReader(private val context: Context, private val publication: Publication) {
 
     private var initialized = false
+    private var paused = false
+
+    private var fullText = mutableListOf<String>()
 
     private val textToSpeech = TextToSpeech(context,
             TextToSpeech.OnInitListener { status ->
                 initialized = (status != TextToSpeech.ERROR)
             })
 
-    
+
+
     private fun isInitialized(): Boolean {
         return initialized
     }
@@ -43,6 +57,28 @@ class R2ScreenReader(private val context: Context, private val publication: Publ
                 Toast.makeText(context.applicationContext, "There was an error with the TTS language, switching to EN-US", Toast.LENGTH_LONG).show()
                 textToSpeech.language = Locale.US
             }
+
+            //checking progression
+            textToSpeech.setOnUtteranceProgressListener(object: UtteranceProgressListener() {
+                override fun onDone(p0: String?) {
+                    //When the current utterance is done spoken, we removed it from the current text
+                    fullText.removeAt(0)
+
+                    if (fullText.isEmpty()) {
+                        stopReading()
+                    }
+                }
+
+                override fun onStart(p0: String?) {
+                    //nothing to do
+                }
+
+                override fun onError(p0: String?) {
+                    //TODO
+                    //Even though it's deprecated, still needed to instantiate UtteranceProgressListener object
+                }
+            })
+
         } else {
             Toast.makeText(context.applicationContext, "There was an error with the TTS initialization", Toast.LENGTH_LONG).show()
         }
@@ -50,25 +86,40 @@ class R2ScreenReader(private val context: Context, private val publication: Publ
 
     fun shutdown() {
         initialized = false
-        textToSpeech.stop()
+
+        stopReading()
+
         textToSpeech.shutdown()
     }
 
-    fun stop() {
-        textToSpeech.stop()
-    }
 
+    fun startReading(text: List<String>) {
+        if (fullText.isEmpty() && !paused) {
+            fullText = text as MutableList<String>
+        }
 
-    fun read(text: String) {
-        val textSplitted = text.split(".")
-
-        for (bitsOfText in textSplitted) {
+        for (bitsOfText in fullText) {
             textToSpeech.speak(bitsOfText, TextToSpeech.QUEUE_ADD, null, "")
         }
     }
 
+    fun pauseReading() {
+        paused = true
+        textToSpeech.stop()
+    }
 
-    fun getText(baseURL: String, epubName: String, resourceHref: String): String {
+    fun resumeReading() {
+        startReading(fullText)
+    }
+
+    fun stopReading() {
+        textToSpeech.stop()
+        paused = false
+        fullText.clear()
+    }
+
+
+    fun getText(baseURL: String, epubName: String, resourceHref: String): List<String> {
         var plainTextFromHTML = ""
 
         val thread = Thread(Runnable {
@@ -89,7 +140,17 @@ class R2ScreenReader(private val context: Context, private val publication: Publ
         thread.start()
         thread.join()
 
-        return plainTextFromHTML
+        return plainTextFromHTML.split(".")
+    }
+
+
+
+    fun isTTSSpeaking(): Boolean {
+        return textToSpeech.isSpeaking
+    }
+
+    fun isNotPaused(): Boolean {
+        return paused
     }
 
 }
